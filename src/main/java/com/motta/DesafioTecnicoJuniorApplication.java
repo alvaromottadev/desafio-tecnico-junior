367a706ff1ddb89d2426a1688f0df4b000bd02cf
motta <alvaromotta.dev@gmail.com>
2025-08-09 18:53:29 -0300

    Initial commit

---
+++ src/main/java/com/motta/DesafioTecnicoJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.motta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoJuniorApplication.class, args);
+	}
+
+}
